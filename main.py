6d9dfe2ad688f9f7edf90292d302a6122ffc4524
jiashengli <jsli@umd.edu>
2022-07-04 22:53:02 -0400

    ajax running and button works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 # import numpy as np
 # import cv2 as cv
 # import timeit
@@ -8,7 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World<"
+    return render_template('index.html')
+
+@app.route('/run_imgMatch')
+def b_test():
+    print("Hello")
+    print("running image Match")
+    return ()
 
 
 if __name__ == '__main__':
