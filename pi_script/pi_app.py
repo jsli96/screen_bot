7551e6d219718b6920fc521be1880ce2093d1ca9
jiashengli <jsli@umd.edu>
2022-07-24 12:02:53 -0400

    IR

--- pi_script/pi_app.py
+++ pi_script/pi_app.py
@@ -11,6 +11,9 @@ MOTOR_B_PHASE = 'GPIO6'    # Phase input for rotation motor
 ROTATION_C1 = 'GPIO21'     # Motor encoder C1
 ROTATION_C2 = 'GPIO20'     # Motor encoder C2
 ROTATION_VCC = 'GPIO16'    # Encoder power line
+IR_1 =
+IR_2 =
+
 # camera = Picamera()
 URL_LOCAL = 'http://127.0.0.1:5000/'
 URL_CLOUD = 'https://screen-bot-proj.herokuapp.com/'
