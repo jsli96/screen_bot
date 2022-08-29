cdafef6d84671d7b94971e0f01dca913160d3a52
jiashengli <jsli@umd.edu>
2022-08-28 22:20:43 -0400

    update

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def index():
 @app.route('/addtocart/')
 def add_to_cart():
     global CAM_POS, P1
-    d, a = get_angle_length([500,200], [312, 500], [100,100])
+    d, a = get_angle_length(CAM_POS, [312, 500], P1)
     print("distance: ", d)
     print("angle: ",a )
     socketio.emit("distance", d)
