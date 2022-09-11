4fc63ee7e2a427c3ba6b3e0efa9e341ab2607017
jiashengli <jsli@umd.edu>
2022-09-11 17:31:28 -0400

    update evaluation images

--- imageMatch.py
+++ imageMatch.py
@@ -112,7 +112,7 @@ def run_app(array_1, array_2, array_3):
     if p1 is not None and p2 is not None and p3 is not None:
         center, r = get_circle(p1, p2, p3)
         # OpenCV evaluation code, comment below if needs run
-        d_1, a_1 = get_angle_length(center, [265, 1000], p1)
+        d_1, a_1 = get_angle_length(center, [1360, 200], p1)
         print("distance_1: ", d_1)
         print("angle_1: ", a_1)
     else:
