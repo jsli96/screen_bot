99a6e43856f529effd865841692345a97f02790d
jiashengli <jsli@umd.edu>
2022-09-11 13:22:40 -0400

    update evaluation images

--- imageMatch.py
+++ imageMatch.py
@@ -55,14 +55,14 @@ def img_match(img_name, img_temp):
     index_params = dict(algorithm=FLANN_INDEX_KD_TREE, trees=5)
     search_params = dict(checks=50)
     flann = cv.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des, des_tem, k=2)
+    matches = flann.knnMatch(des, des_tem, k=3)
     #BF match
     # bf = cv.BFMatcher()
     # matches = bf.knnMatch(des, des_tem, k=2)
     good = []
     # Filter results
     for m, n in matches:
-        if m.distance < 0.75 * n.distance:
+        if m.distance < 0.7 * n.distance:
             good.append(m)
 
     if len(good) > MIN_MATCH_COUNT:
@@ -95,15 +95,15 @@ def run_app(array_1, array_2, array_3):
     img_template = cv.imread("photo/evaluation_template_2.jpg", cv.IMREAD_GRAYSCALE)  # Read template image
     # img_template_color = cv.imread("photo/test_template_dot.jpeg", cv.IMREAD_COLOR)
     img1 = cv.imdecode(array_1, cv.IMREAD_GRAYSCALE)  # Read first image
-    img1 = cv.resize(img1, (0, 0), fx=0.5, fy=0.5)
+    img1 = cv.resize(img1, (0, 0), fx=1, fy=1)
     p1 = img_match(img1, img_template)
     img1 = 0
     img2 = cv.imdecode(array_2, cv.IMREAD_GRAYSCALE)  # Read second image
-    img2 = cv.resize(img2, (0, 0), fx=0.5, fy=0.5)
+    img2 = cv.resize(img2, (0, 0), fx=1, fy=1)
     p2 = img_match(img2, img_template)
     img2 = 0
     img3 = cv.imdecode(array_3, cv.IMREAD_GRAYSCALE)  # Read third image
-    img3 = cv.resize(img3, (0, 0), fx=0.5, fy=0.5)
+    img3 = cv.resize(img3, (0, 0), fx=1, fy=1)
     p3 = img_match(img3, img_template)
     img3 = 0
     stop_1 = timeit.default_timer()
